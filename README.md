58b8df8152166b5c36c4549746c1fc255bb6c49b
Alexander <info@alex-j.net>
2021-03-18 20:33:02 +0100

    initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# Playbook play_hashicorp_vault
+
+```bash
+
+rm -rf venv/
+rm -rf /root/.cache/pip/
+
+mkdir -p venv
+python3 -m venv --system-site-packages venv
+source venv/bin/activate
+
+pip3 install --upgrade pip
+pip3 install -r requirements.txt --upgrade
+
+# ansible-galaxy install -v -f -r roles/requirements.yml
+
+export ANSIBLE_LOG_PATH="ansible_$(date +"%y%m%d%H%M%S").log"
+echo "Logdatei: ${ANSIBLE_LOG_PATH}"
+
+ansible --version | head -n 1
+ansible --version | grep "python version"
+
+ansible-playbook -i inventories/test/hosts.ini -v playbook-test.yml
+```
---
+++ files/sqlite3_test.py
@@ -0,0 +1,39 @@
+import sqlite3
+from sqlite3 import Error
+import json
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def select_foo(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM foo")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    return json.dumps(rows)
+
+
+def main():
+    database = "sqlite.db"
+
+    conn = create_connection(database)
+    with conn:
+        print("Select foo")
+        output_json = select_foo(conn)
+        print(output_json)
+
+
+if __name__ == '__main__':
+    main()
